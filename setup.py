08b85db5aeebd58fb9cf9d0fdc2e93cf107b8555
谭九鼎 <109224573@qq.com>
2020-10-21 13:51:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setuptools.setup(
     version="1.0.1",
     py_modules=['movies','movieSource.MovieHeaven','movieSource.fake_user_agent'],
     entry_points={'gui_scripts': ['MovieHeavens = movies:_main']},
+    install_requires=['PyQt5','requests','fake-useragent'],
     include_package_data=True
 )
