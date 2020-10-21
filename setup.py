84536f70c75b91bf7f463bf65449000190e78b45
谭九鼎 <109224573@qq.com>
2020-10-21 13:46:28 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="MovieHeavens",
+    version="1.0.1",
+    py_modules=['movies','movieSource.MovieHeaven','movieSource.fake_user_agent'],
+    entry_points={'gui_scripts': ['MovieHeavens = movies:_main']},
+    include_package_data=True
+)
